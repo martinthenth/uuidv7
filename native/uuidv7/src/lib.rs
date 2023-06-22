e66749e4d1a8ea31d97a5b85468d0a049ee6afdb
Martin Nijboer <mrnijboer@gmail.com>
2023-06-22 21:08:32 +0200

    Generate v7 UUIDs

--- native/uuidv7/src/lib.rs
+++ native/uuidv7/src/lib.rs
@@ -8,7 +8,7 @@ fn add(a: i64, b: i64) -> i64 {
 #[rustler::nif]
 fn generate() -> String {
     // Uuid::new_v7().to_string()
-    Uuid::new_v4().to_string()
+    Uuid::now_v7().to_string()
 }
 
 rustler::init!("Elixir.UUIDv7", [add, generate]);
