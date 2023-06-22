0cc6cf879010ca0ad82b7cc727f4eee2afeca19c
Martin Nijboer <mrnijboer@gmail.com>
2023-06-22 20:44:45 +0200

    Generate string UUIDv4

--- native/uuidv7/src/lib.rs
+++ native/uuidv7/src/lib.rs
@@ -1,6 +1,14 @@
+use uuid::Uuid;
+
 #[rustler::nif]
 fn add(a: i64, b: i64) -> i64 {
     a + b
 }
 
-rustler::init!("Elixir.UUIDv7", [add]);
+#[rustler::nif]
+fn generate() -> String {
+    // Uuid::new_v7().to_string()
+    Uuid::new_v4().to_string()
+}
+
+rustler::init!("Elixir.UUIDv7", [add, generate]);
--- test/uuidv7_test.exs
+++ test/uuidv7_test.exs
@@ -6,4 +6,12 @@ defmodule UUIDv7Test do
       assert UUIDv7.add(1, 3) == 4
     end
   end
+
+  describe "generate/0" do
+    test "generates an UUID" do
+      assert is_binary(UUIDv7.generate())
+
+      IO.inspect(UUIDv7.generate())
+    end
+  end
 end
