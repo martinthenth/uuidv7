ca40791148d1d6a31fbbdd30e0c5c52f0c66ada2
Martin Nijboer <mrnijboer@gmail.com>
2023-06-22 20:44:45 +0200

    Generate string UUIDv4

--- lib/uuidv7.ex
+++ lib/uuidv7.ex
@@ -16,4 +16,9 @@ defmodule UUIDv7 do
   """
   # When your NIF is loaded, it will override this function.
   def add(_a, _b), do: :erlang.nif_error(:nif_not_loaded)
+
+  @doc """
+  Hello world.
+  """
+  def generate(), do: :erlang.nif_error(:nif_not_loaded)
 end
