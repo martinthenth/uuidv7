b1482939e3a435406d6af799d049be2e69c6fb78
Martin Nijboer <mrnijboer@gmail.com>
2023-06-22 19:36:05 +0200

    Initial commit

---
+++ lib/uui_dv7.ex
@@ -0,0 +1,18 @@
+defmodule UUIDv7 do
+  @moduledoc """
+  Documentation for `UUIDv7`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> UUIDv7.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/uui_dv7_test.exs
@@ -0,0 +1,8 @@
+defmodule UUIDv7Test do
+  use ExUnit.Case
+  doctest UUIDv7
+
+  test "greets the world" do
+    assert UUIDv7.hello() == :world
+  end
+end
