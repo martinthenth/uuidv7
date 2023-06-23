d76b31718948767f0c9fd10d049988e73023c452
Martin Nijboer <mrnijboer@gmail.com>
2023-06-23 18:17:29 +0200

    Add license, changelog and benchmark

--- mix.exs
+++ mix.exs
@@ -1,22 +1,29 @@
 defmodule UUIDv7.MixProject do
   use Mix.Project
 
+  @version "0.1.0"
+  @source_url "https://github.com/martinthenth/uuidv7"
+  @changelog_url "https://github.com/martinthenth/uuidv7/blob/main/CHANGELOG.md"
+
   def project do
     [
       app: :uuidv7,
-      version: "0.1.0",
+      version: @version,
       elixir: "~> 1.13",
       start_permanent: Mix.env() == :prod,
-      deps: deps()
+      deps: deps(),
+      description: "A UUIDv7 implementation and Ecto.Type for Elixir - based on Rust",
+      source_ref: @version,
+      source_url: @source_url,
+      docs: docs(),
+      package: package()
     ]
   end
 
-  # Run "mix help compile.app" to learn about applications.
   def application do
     [extra_applications: [:logger]]
   end
 
-  # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
       {:rustler, "~> 0.29.0"},
@@ -26,6 +33,21 @@ defmodule UUIDv7.MixProject do
       {:ex_doc, "~> 0.27", only: :dev, runtime: false},
       {:benchee, "~> 1.1", only: :dev},
       {:uniq, "~> 0.1", only: :dev}
+    ]
+  end
+
+  defp package do
+    [
+      maintainers: ["Martin Nijboer"],
+      licenses: ["MIT"],
+      links: %{"GitHub" => @source_url, "Changelog" => @changelog_url}
+    ]
+  end
+
+  defp docs do
+    [
+      main: "UUIDv7",
+      extras: ["README.md"]
     ]
   end
 end
